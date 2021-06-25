d70234796d538eeda85874a5391b91023dbb52e4
Wojciech Olasiewicz <wojciech.olasiewicz@corp.smtsoftware.com>
2021-06-25 11:43:13 +0200

    updated gitignore

--- app/src/main/java/com/example/cryptorxjavaroomretrofit/model/service/ApiService.kt
+++ app/src/main/java/com/example/cryptorxjavaroomretrofit/model/service/ApiService.kt
@@ -1,12 +1,13 @@
 package com.example.cryptorxjavaroomretrofit.model.service
 
+import com.example.cryptorxjavaroomretrofit.BuildConfig
 import com.example.cryptorxjavaroomretrofit.model.Crypto
 import io.reactivex.Single
 import retrofit2.http.GET
 
 interface ApiService {
 
-    @GET("prices?key=ad1c827dda8c777fbc36e7268dfa123b674fb862")
+    @GET("prices?key=${BuildConfig.API_KEY}")
     fun getData() : Single<List<Crypto>>
 
 }
